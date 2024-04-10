8dbe20f2889290b62f145f758da3c83411ce2a16
darkzpvp <victor1val@hotmail.es>
2024-04-10 16:30:43 +0200

    Arreglado una cosa del formulario

--- src/components/FormularioCarrito.tsx
+++ src/components/FormularioCarrito.tsx
@@ -34,7 +34,7 @@ const FormularioCarrito = ({continuar, setContinuar}) => {
           <input
             type="text"
             id="nombre"
-            className="bg-gray-600/50 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500  "
+            className="bg-gray-700 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500  "
             placeholder="Víctor"
             onChange={nombre}
             required
@@ -52,7 +52,7 @@ const FormularioCarrito = ({continuar, setContinuar}) => {
           <input
             type="text"
             id="nombre"
-            className="bg-gray-600/50 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
+            className="bg-gray-700 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
             placeholder="Valverde"
             onChange={nombre}
             required
@@ -68,7 +68,7 @@ const FormularioCarrito = ({continuar, setContinuar}) => {
           <input
             type="tel"
             id="telefono"
-            className="bg-gray-600/50 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
+            className="bg-gray-700 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
             placeholder="654958823"
             onChange={numero}
             required
@@ -85,7 +85,7 @@ const FormularioCarrito = ({continuar, setContinuar}) => {
 
           <select
             defaultValue={`ES`}
-            className="bg-gray-600/50 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
+            className=" text-sm rounded-lg  block w-full p-2.5 bg-gray-700 placeholder-gray-500 text-gray-300  "
           >
             <option value="" disabled>
               --Selecciona un país--
@@ -108,7 +108,7 @@ const FormularioCarrito = ({continuar, setContinuar}) => {
           <input
             type="text"
             id="last_name"
-            className="bg-gray-600/50 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
+            className="bg-gray-700 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
             placeholder="Málaga"
             onChange={nombre}
             required
@@ -124,7 +124,7 @@ const FormularioCarrito = ({continuar, setContinuar}) => {
           <input
             type="text"
             id="last_name"
-            className="bg-gray-600/50 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500  "
+            className="bg-gray-700 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500  "
             placeholder="Málaga"
             onChange={nombre}
             required
@@ -140,12 +140,19 @@ const FormularioCarrito = ({continuar, setContinuar}) => {
           <input
             type="text"
             id="last_name"
-            className="bg-gray-600/50 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
+            className="bg-gray-700 text-gray-300 text-sm rounded-lg  block w-full p-2.5  placeholder-gray-500   "
             placeholder="44332255G"
             required
           />
         </div>
       </div>
+      <div className="flex items-center mt-5 text-gray-400 text-sm gap-2 fill-gray-400">
+          <p>Tus datos se encuentran protegidos según la Ley de protección de datos</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 0 1-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 0 1-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 0 1-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584ZM12 18a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
+</svg>
+
+        </div>
     </form>
   </div>
   )
