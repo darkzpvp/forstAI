855eff835d4290d4337286162c77ba0cc2bb6919
darkzpvp <victor1val@hotmail.es>
2024-04-25 18:09:33 +0200

    Agregado skeleton, añadido mejoras de funcionalidad

---
+++ app/generar/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+
+import "../globals.css";
+import { Kumbh_Sans} from 'next/font/google'
+const kumbh = Kumbh_Sans({
+  subsets: ['latin'],
+  variable: '--font-kumbh',
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "Generador de imágenes",
+  description: "Generador de imágenes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  
+  return (
+    <html lang="en" className=" overflow-x-hidden">
+      
+      <body className={`${kumbh.className}`}>
+
+  {children}
+
+     
+
+        </body>
+    </html>
+  );
+}
