6022d73591f7472b5be79332129ea9494589f86d
darkzpvp <victor1val@hotmail.es>
2024-04-30 20:24:58 +0200

    Validación contacto

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -1,29 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Notifications\RecibirEmailNotificacion;
 use Illuminate\Support\Facades\Notification;
-use Illuminate\Http\Request;
-
+use Illuminate\Validation\ValidationException;
+use App\Http\Requests\EmailRequest;
 class EmailController extends Controller
 {
-    public function receiveEmail(Request $request)
-{
-    // Validar la solicitud
-    $validatedData = $request->validate([
-        'name' => 'required|string',
-        'email' => 'required|email',
-        'message' => 'required|string',
-    ]);
+    public function receiveEmail(EmailRequest $request)
+    {
+        $data = $request->validated();
+     
 
-    // Procesar el correo electrónico recibido
 
-    // Enviar notificación por correo electrónico
-    $emailData = $validatedData;
-    Notification::route('mail', 'your-email@example.com')->notify(new RecibirEmailNotificacion($emailData));
+        $emailData = $data;
+        Notification::route('mail', 'your-email@example.com')->notify(new RecibirEmailNotificacion($emailData));
 
-    return response()->json([
-        'status' => 'success',
-    ]);
-}
+        return response()->json([
+            'status' => '¡Correo enviado con éxito!',
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -9,6 +8,8 @@ use App\Http\Controllers\EmailVerificationController;
 use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\ResetPasswordController;
 use App\Http\Controllers\EmailController;
+
+// Routes requiring authentication
 Route::middleware(['auth:sanctum', 'verified'])->group(function(){
     Route::get('/user', function (Request $request){
         return $request->user();
@@ -17,23 +18,13 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
     Route::post('/enviar_formulario', [PromptController::class, 'enviarFormulario']);
     Route::get('/prompts', [PromptController::class, 'getPrompts']);
     Route::post('/change-password', [AuthController::class, 'changePassword']);
+    Route::post('email/verification-notification', [EmailVerificationController::class, 'sendVerificationEmail']);
+    Route::get('verify-email/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
 });
 
-
+// Public routes without authentication middleware
 Route::post('/registro', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
-
-
-Route::post('email/verification-notification', [EmailVerificationController::class, 'sendVerificationEmail'])->middleware('auth:sanctum');
-Route::get('verify-email/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify')->middleware('auth:sanctum');
-
-
-
 Route::post('/forgot', [AuthController::class, 'forgot']);
 Route::post('/reset', [AuthController::class, 'reset']);
-
-
-
 Route::post('/receive-email', [EmailController::class, 'receiveEmail']);
-
